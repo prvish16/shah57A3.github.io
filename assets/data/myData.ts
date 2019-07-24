a04a5c71159aeb8029c166e20f71d2e0d100c7ec
prvish16 <45217310+prvish16@users.noreply.github.com>
2019-07-23 20:10:07 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,5 @@
+import { shah57 } from 'src/app/shah57';
+
+export const myInfo: shah57={
+    studentNumber:991498278, name: 'Vishwa', loginName: 'shah57', campus: 'Davis', assignmentTitle: 'Angular Intro'
+  }
---
+++ assets/data/mySongs.ts
@@ -0,0 +1,15 @@
+import { songInfo } from 'src/app/songInfo';
+
+export const song1: songInfo={
+    songName: 'Perfect', artist:'Ed Sheeran', genre:'Pop', yearRealeased: 2017, picture:'./assets/images/ed.jpg'
+}
+
+export const song2: songInfo={
+    songName: 'Thunder', artist:'Imagine Dragons', genre:'Pop', yearRealeased: 2017, picture:'./assets/images/thunder.jpg'
+}
+export const song3: songInfo={
+    songName: 'Fight Song', artist:'Rachel Platten', genre:'Pop', yearRealeased: 2014, picture:'./assets/images/fight.jpg'
+}
+export const song4: songInfo={
+    songName: 'God Plan', artist:'Drake', genre:'Pop', yearRealeased: 2018, picture:'./assets/images/god.jpg'
+}
